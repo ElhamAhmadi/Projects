706a0e47af8e2333b09312ef1335c1774f5c0e0a
eli <elham.ahmaadi@gmail.com>
2022-04-28 23:00:18 -0700

    five

--- First_Project/src/app/app.component.ts
+++ First_Project/src/app/app.component.ts
@@ -6,23 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-   username: string ='' ;
-  pass: string ='';
-  repass: string ='';
-  ismach: boolean = false;
-
-public changeUserName(e: Event){
-    this.username =(<HTMLInputElement>e.target).value;
-
-}
-public changePassword(e: Event){
-    this.pass =(<HTMLInputElement>e.target).value;
-   }
-
-public changeRepassword(e: Event){
-  this.repass =(<HTMLInputElement>e.target).value; 
-  this.ismach = this.pass!== this.repass ? false: true;
-
-       }
+  
 
 }
--- First_Project/src/app/app.module.ts
+++ First_Project/src/app/app.module.ts
@@ -3,16 +3,22 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { MenuComponent } from './menu/menu.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoginComponent,
+    MenuComponent,
+    ContentComponent,
     
     
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
